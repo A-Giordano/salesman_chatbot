0d573f798ad621a747bfe8a3f4608d98889554a1
A-Giordano <andrea.giordano991@gmail.com>
2023-03-14 22:46:20 +0100

    v1

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 
-openai.api_key = st.secrets["OPENAI_KEY"]
+# openai.api_key = st.secrets["OPENAI_KEY"]
+openai.api_key = "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"
 
+# def clear_cache():
+#     st.session_state['customer_conv'] = []
+#     # print(st.session_state)
+#     # st.experimental_rerun()
+#
+#
+# st.button("Restart",on_click=clear_cache)
 
 def generate_response(messages):
+    print('customer_conv---------------\n',messages,'\n-------------')
+
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages=messages,
@@ -25,8 +35,10 @@ st.title("Sales Coach Trainer")
 
 if 'customer_conv' not in st.session_state:
     st.session_state['customer_conv'] = [
-        {"role": "system", "content": "You are a chat bot acting like a customer interested in buying a pen"},
-        # {"role": "user", "content": "How can I help you today?"},
+        {"role": "system",
+         "content": "You are an assistant acting as customer in a sale simulation while the user is trying to sell you a pen"},
+        {"role": "user",
+         "content": "Act as a customer while i'm trying to sell you a pen, start by asking if I sell any pens"}
     ]
     response = generate_response(st.session_state.customer_conv)
     st.session_state.customer_conv.append(response)
@@ -45,7 +57,6 @@ user_input = st.text_input('You:',key='input')
 
 if user_input:
     st.session_state.customer_conv.append({"role": "user", "content": user_input})
-    print('customer_conv---------------\n',st.session_state.customer_conv,'\n-------------')
     response = generate_response(st.session_state.customer_conv)
     st.session_state.customer_conv.append(response)
 
@@ -57,14 +68,13 @@ if st.session_state['customer_conv']:
     # for i in range(len(st.session_state['generated']) - 1, -1, -1):
     #     message(st.session_state["generated"][i], key=str(i))
     #     message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-    message("Hi, I'm a potential customer interested in buying a pen, try to sell it to me in 3 interactiion and then you'll have feedbackss on how well you performed",
+    message("Hi, I'm a potential customer interested in buying a pen, try to sell it to me in 3 interaction and then you'll have feedbacks on how well you performed",
             key="000")
 
     for i, text in enumerate(st.session_state['customer_conv'][2:]):
         if text['role'] == 'user':
             message(text['content'], is_user=True,  key=str(i) + text['role'])
         else:
-            print('text:', text)
             message(text['content'], key=str(i) + text['role'])
 
     if len(st.session_state['customer_conv']) >= 9:
@@ -78,7 +88,7 @@ if st.session_state['customer_conv']:
         # st.session_state.customer_conv.append(feedback)
 
         st.session_state.customer_conv.append(
-            {"role": "user", "content": "provide me a score between 0 and 2 also based on the previous convrsation?"}
+            {"role": "user", "content": """provide feedback on how effectively did I tried to sell the pen, then on a new line give me float score between 0 and 10 in the format of: Score:[float]"""}
         )
         score = generate_response(st.session_state.customer_conv)
         message(score['content'], key="score" + score['role'])
